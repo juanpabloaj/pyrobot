3fefc535fcb234659ed7d58cfecfa0e0109792a5
JuanPablo <jpabloaj@gmail.com>
2016-02-17 23:02:05 -0300

    a base setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="py-robot",
+    version="0.1.0",
+    description="A pure python windows automation library",
+    license="MIT",
+    author="Chris Kiehl",
+    author_email='audionautic@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
